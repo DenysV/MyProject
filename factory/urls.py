090dbb2115ae1690091c3f16b5e63f0229826283
DenysV <denzelSpain@gmail.com>
2018-07-06 17:57:46 +0200

    Block templates

--- factory/urls.py
+++ factory/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from . import views
 urlpatterns = [
-    url(r'^$', views.home, name = 'home'),
-    url(r'^login', views.login1, name = 'login1'),
+    url(r'^login', views.home, name = 'home'),
+    url(r'^projects', views.login1, name = 'login1'),
     #url(r'^projects/$', views.projects, name = 'projects'),
     url(r'^project/(?P<pk>\d+)/tareas/$', views.index1, name = 'index1'),
     #url(r'^project/(?P<pk>\d+)/tareas/$', views.projects_back, name = 'projects_back'),
